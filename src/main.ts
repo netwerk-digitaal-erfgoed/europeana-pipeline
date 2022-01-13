fb3c073b99666c072ba191f8337d79b5ed668049
Thomas de Groot <thomas.de.groot@triply.cc>
2022-01-13 14:54:08 +0100

    removed complexity and moved SPARQL constructs to a RATT only basis.

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,38 @@
-import { Ratt, CliContext } from "@triply/ratt";
+import { Ratt, CliContext, Context } from "@triply/ratt";
 import mw from "@triply/ratt/lib/middlewares";
 import {
   prefixes,
   retrieveDatasetsQueryString,
   retrieveDatasetMetadata,
   retrieveDatasetMetadataQueryString,
+  retrieveInstances,
+  eccbooks2edm,
+  eccbooks2edmQueryString,
+  eccucfix2edm,
+  eccucfix2edmQueryString,
+  finnabooks2edm,
+  finnabooks2edmQueryString,
+  ksamsok2edm,
+  ksamsok2edmQueryString,
+  nmvw2edm,
+  nmvw2edmQueryString,
+  schema2edm,
+  schema2edmQueryString,
 } from "./helpers/generics";
-import {
-  endpoint,
-  rattBindings,
-  constructQueries,
-} from "./helpers/etl-helpers";
-import { forEachiterator, externalSPARQL } from "./helpers/ratt-helpers";
+import { IQueryResult } from "@comunica/actor-init-sparql/lib/ActorInitSparql-browser";
+import { newEngine } from "@triply/actor-init-sparql-rdfjs";
+import * as path from "path";
+import { streamToString } from "@triply/ratt/lib/utils/files";
+const md5 = require("md5");
+
+import { Parser } from "n3";
+export declare type CompressionType = "gz" | undefined;
 
 // Record
+const contentLength = "content-length";
 const datasetIri = "?datasetIri";
 const dataUrl = "?dataUrl";
 const dataService = "?dataService";
-const instanties = "?instanties";
-const dsType = "?type";
 const datasetRegister = "?datasetRegister";
 // Misc
 
@@ -43,9 +57,7 @@ const sources = {
   schemaShapes: Ratt.Source.file(
     "./rdf/informatieModellen/shacl_dataset_dump_schema.ttl"
   ),
-  edmShapes: Ratt.Source.file(
-    "./rdf/informatieModellen/shacl_edm.ttl"
-  ),
+  edmShapes: Ratt.Source.file("./rdf/informatieModellen/shacl_edm.ttl"),
 };
 
 const destinations = {
@@ -61,7 +73,7 @@ const destinations = {
 
 export default async function (cliContext: CliContext): Promise<Ratt> {
   // RATT context
-  const app = new Ratt({
+  const pipe1 = new Ratt({
     defaultGraph: prefixes.defaultGraph,
     cliContext: cliContext,
     prefixes: prefixes,
@@ -75,65 +87,189 @@ export default async function (cliContext: CliContext): Promise<Ratt> {
   //    dataUrl: 'http://data.bibliotheken.nl/sparql',
   //    dataFormat: 'application/sparql-query',
   // }
-  app.use(mw.fromCsv(app.sources.datasetCatalog, { delimiter: "\t" }));
-
-  // TD issue: https://issues.triply.cc/issues/5616
-  // Inladen van de metadata van de datasets die in de dataset catalogus van NDE beschikbaar zijn.
-  app.use(
-    mw.add({
-      key: retrieveDatasetMetadata,
-      value: (ctx) => {
-        return retrieveDatasetMetadataQueryString.replace(
-          "?dataset",
-          ctx.getString(datasetIri)
-        );
-      },
-    }),
-    mw.add({
-      key: datasetRegister,
-      value: (_ctx) => datasetRegisterValue,
-    }),
-    externalSPARQL(datasetRegister, retrieveDatasetMetadata)
+  pipe1.use(mw.fromCsv(pipe1.sources.datasetCatalog, { delimiter: "\t" }));
+  pipe1.use(
+    async (ctx, next) => {
+      const parser = new Parser();
+      const query = retrieveDatasetMetadataQueryString.replace(
+        "?dataset",
+        ctx.getString(datasetIri)
+      )
+      const response = await fetch(`${datasetRegisterValue}`, {
+        headers: {
+          accept: "text/turtle",
+          "content-type": "application/sparql-query",
+        },
+        body: query,
+        method: "POST",
+      });
+      const text = await response.text()
+      ctx.store.addQuads(parser.parse(text));
+      return next();
+    }
   );
 
   // validatie van de metadata van de datasets
-  app.use(
-    mw.validateShacl([app.sources.dcatShapes, app.sources.schemaShapes], {
-      report: { destination: app.destinations.report, graph: reportGraph },
+  pipe1.use(
+    mw.validateShacl([pipe1.sources.dcatShapes, pipe1.sources.schemaShapes], {
+      report: { destination: pipe1.destinations.report, graph: reportGraph },
       terminateOn: false,
     })
   );
 
-  app.use(
-    mw.when(dataUrl, [
-      // Reset de store om leeg te beginnen voor het inhoudelijke gedeelte.
-      mw.resetStore(),
-      // Aanmaken van de SPARQL interface om de bevragingen over uit te voeren.
-      mw.add({ key: dataService, value: endpoint }),
-    ])
-  );
-
-  app.use(
-    mw.when(
-      (ctx) => ctx.isNotEmpty(dsType) && ctx.getString(dsType) === "RATT",
-      // Ophalen instanties
-      mw.add({ key: instanties, value: rattBindings }),
-      forEachiterator(instanties, [
-        mw.add({
-          key: "instance",
-          value: (ctx) => {
-            return "<" + ctx.getString("_root.entries[0][1].value") + ">";
+  pipe1.use(
+    // Aanmaken van de SPARQL interface om de bevragingen over uit te voeren.
+    mw.add({
+      key: dataService,
+      value: async (ctx) => {
+        try {
+          const url = ctx.getString(dataUrl).replace("<", "").replace(">", "");
+          const head = await fetch(`${url}`, { method: "HEAD" });
+          // const size = head.headers.get(contentLength);
+          const parser = new Parser();
+          const response = await fetch(`${url}`);
+          if (response.body) {
+            const rdfStream: any = response.body;
+            let extension = path.extname(url);
+            let compression: CompressionType;
+            if (extension === ".gz") {
+              compression = "gz";
+            } else if (
+              head.headers.get("Content-Encoding") === "application/gzip"
+            ) {
+              compression = "gz";
+            }
+            const datasetStore = ctx.app.getNewStore();
+            datasetStore.addQuads(
+              parser.parse(await streamToString(rdfStream, compression))
+            );
+            if (datasetStore.size === 0) return false
+            ctx.store = datasetStore
+            return true;
+          }
+        } catch (error: any) {
+          console.warn(
+            error.message,
+            `RATT SPARQL endpoint from dataset ${ctx.record[datasetIri]} could not be created`
+          );
+          return false
+        }
+      },
+    }),
+    mw.when(dataService, [
+      async (ctx, next) => {
+        const graph = ctx.getString(datasetIri).replace("<", "").replace(">", "")
+        const mainStore = ctx.store
+        const pipe2 = new Ratt({
+          defaultGraph: graph,
+          cliContext: cliContext,
+          sources: sources,
+          destinations: {
+            dataset: Ratt.Destination.TriplyDb.rdf(md5(ctx.getString(dataUrl)),{overwrite:true}),
           },
-        }),
-        constructQueries(),
-        mw.validateShacl([app.sources.edmShapes], {
-          report: { destination: app.destinations.report, graph: reportGraph },
-          terminateOn: false,
-        }),
-        mw.toRdf(app.destinations.dataset)
-      ])
-    ),
+        });
+        let queryResult: IQueryResult;
+        const engine = newEngine();
+        try {
+          queryResult = await engine.query(retrieveInstances, {
+            sources: [mainStore],
+          });
+        } catch (e) {
+          if (e instanceof Error && e.message.startsWith("Parse error")) {
+            e.message = "Failed to parse query: " + e.message;
+          }
+          throw e;
+        }
+        if (queryResult.type === "bindings") {
+          pipe2.use(
+            mw.fromJson(await queryResult.bindings()),
+            (ctx,next)=>{
+              ctx.store = mainStore
+              return next()
+            },
+            mw.add({
+              key: "instance",
+              value: (ctx) => {
+                return "<" + (ctx.record as any).get("?uri").value + ">";
+              },
+            }),
+            mw.add({
+              key: eccbooks2edm,
+              value: (ctx: Context) => {
+                return eccbooks2edmQueryString.replace(
+                  /\?id/g,
+                  ctx.getString("instance")
+                );
+              },
+            }),
+            mw.add({
+              key: eccucfix2edm,
+              value: (ctx: Context) => {
+                return eccucfix2edmQueryString.replace(
+                  /\?id/g,
+                  ctx.getString("instance")
+                );
+              },
+            }),
+            mw.add({
+              key: finnabooks2edm,
+              value: (ctx: Context) => {
+                return finnabooks2edmQueryString.replace(
+                  /\?id/g,
+                  ctx.getString("instance")
+                );
+              },
+            }),
+            mw.add({
+              key: ksamsok2edm,
+              value: (ctx: Context) => {
+                return ksamsok2edmQueryString.replace(
+                  /\?id/g,
+                  ctx.getString("instance")
+                );
+              },
+            }),
+            mw.add({
+              key: nmvw2edm,
+              value: (ctx: Context) => {
+                return nmvw2edmQueryString.replace(/\?id/g, ctx.getString("instance"));
+              },
+            }),
+            mw.add({
+              key: schema2edm,
+              value: (ctx: Context) => {
+                return schema2edmQueryString.replace(
+                  /\?id/g,
+                  ctx.getString("instance")
+                );
+              },
+            }),
+            mw.sparqlConstruct((ctx)=>ctx.getString(eccbooks2edm)),
+            mw.sparqlConstruct((ctx)=>ctx.getString(eccucfix2edm)),
+            mw.sparqlConstruct((ctx)=>ctx.getString(finnabooks2edm)),
+            mw.sparqlConstruct((ctx)=>ctx.getString(ksamsok2edm)),
+            mw.sparqlConstruct((ctx)=>ctx.getString(nmvw2edm)),
+            mw.sparqlConstruct((ctx)=>ctx.getString(schema2edm)),
+            mw.validateShacl([pipe2.sources.edmShapes], {graphs: [graph],
+              report: {
+                destination: pipe2.destinations.dataset,
+                graph: reportGraph,
+              },
+              log: false,
+              terminateOn: false,
+            }),
+            mw.toRdf(pipe2.destinations.dataset, {includeGraphs: [graph]})
+          );
+          pipe2.after(()=>{
+            pipe2
+          })
+          await pipe2.run();
+        }
+        return next();
+      },
+    ],
+  mw.logRecord())
   );
 
-  return app;
+  return pipe1;
 }
